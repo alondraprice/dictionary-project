7f2bf1d55e77fdcea9ff3d72f5f02017fba94f7b
alondraprice <98491739+alondraprice@users.noreply.github.com>
2022-10-24 12:02:39 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logobook.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dictionary</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      <a href="#" className="btn btn-primary shadow">Hello</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
