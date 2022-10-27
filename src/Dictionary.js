23dd56784c78aee5ee1320f0a956f5c2701819f0
alondraprice <98491739+alondraprice@users.noreply.github.com>
2022-10-27 11:50:40 -0500

    word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary(){
-    let[keyword, setKeyword]= useState("")
+    let[keyword, setKeyword]= useState("");
+    let[results, setResults]= useState(null);
 
     function handleResponse(response){
+        setResults(response.data[0]);
     }
     
     function search(event) {
-        event.preventDefault()
+        event.preventDefault();
 
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
@@ -21,9 +24,10 @@ export default function Dictionary(){
 
     return (
     <div className="Dictionary">
-        <form onSumbit={search}>
+        <form onSubmit={search}>
             <input type="search" onChange={handleKeywordChange} />
         </form>
+        <Results results={results} />
     </div>
     );
 }
